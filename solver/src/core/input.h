ed3a315b06619b0c30cc9e2ebcd0ba5ef8966453
Guillaume Tauzin <guillaumetauzin.ut@gmail.com>
2017-04-05 10:06:45 +0200

    Separated enum class options and input

--- solver/src/core/input.h
+++ solver/src/core/input.h
@@ -5,26 +5,11 @@
 #include <vector>
 #include <string>
 
+#include "options.h"
 
 namespace lbm {
-  enum class LatticeType {D1Q3, D2Q5, D2Q9, D3Q15, D3Q19, D3Q27};
-
-  enum class SolverMethod {BGK, ELBM, Approached_ELBM, ForcedNR_ELBM, ForcedBNR_ELBM, error};
-
-  enum class InitDensityType{homogeneous, peak, error};
-  enum class InitVelocityType{homogeneous, perturbated, wave, decay, error};
-
-  enum class ForcingSchemeMethod {guo, SC, EDM, error};
-  enum class ForceType {constant, sinusoidal, kolmogorov, turbulent, turbulent_time, wavenumbers_time, error};
-
-  enum class BoundaryType {bounceBack_halfWay, pressure_ZouHe, velocity_ZouHe, entropic, corner, error};
-  enum class BoundaryPosition {top, bottom, left, right, top_left, top_right, bottom_right, bottom_left, error};
-  enum class CornerPosition {top_left, top_right, bottom_right, bottom_left, error};
-
-  enum class OutputType {vtr, backup, error};
-
   typedef DATA_TYPE valueType;
-  constexpr LatticeType latticeType = LatticeType::D3Q15;
+  constexpr LatticeType latticeType = LatticeType::D1Q3;
   constexpr int lengthX_g = 16;
   constexpr int lengthY_g = 16;
   constexpr int lengthZ_g = 16;
