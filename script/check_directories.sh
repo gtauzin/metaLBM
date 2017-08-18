275e7252ac4da5e700d1dd3623d22023be9d1be0
Guillaume Tauzin <guillaumetauzin.ut@gmail.com>
2017-08-18 10:58:38 +0200

    Revised repo structure

--- script/check_directories.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../$1
-mkdir -p ../output ../output/outputVTR ../output/outputBackup ../output/outputPlot ../output/outputAnimation> /dev/null
-mkdir -p ../input/inputJSON > /dev/null
-mkdir -p build bin log docs > /dev/null
