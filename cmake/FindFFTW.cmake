e655b53b50cd4d9834bcda071631c0da2e2a08ed
Guillaume Tauzin <guillaumetauzin.ut@gmail.com>
2018-02-06 17:20:53 +0100

    Added CMake capabilities for HDF5 and FFTW

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -1,79 +1,71 @@
-# if environment variable FFTWDIR is specified, it has same effect as FFTW_ROOT
-if( NOT FFTW_ROOT AND DEFINED ENV{FFTWDIR} )
-    set( FFTW_ROOT $ENV{FFTWDIR} )
+# Copyright Guillaume Tauzin 2018
+# Distributed under the MIT License (MIT)
+
+# .rst:
+# FindFFTW
+# ------------
+#
+# Locate the FFT library FFTW
+#
+#
+# Imported targets
+# ^^^^^^^^^^^^^^^^
+#
+# ``FFTW::FFTW``
+#
+#
+# Result variables
+# ^^^^^^^^^^^^^^^^
+#
+# This module will set the following variables in your project:
+#
+# ``FFTW_FOUND``            - Found the FFTW library
+# ``FFTW_INCLUDE_DIRS``     - FFTW include directories
+#
+#
+# Cache variables
+# ^^^^^^^^^^^^^^^
+#
+# The following Cache variables may also be set:
+#
+# ``FFTW_ROOT``             - The root directory of FFTW installation
+#                                 (may also be set as an environment variable)
+#
+
+if(NOT FFTW_ROOT AND (DEFINED ENV{FFTW_ROOT}))
+  set(FFTW_ROOT $ENV{FFTW_ROOT})
 endif()
-# Check if we can use PkgConfig
-find_package(PkgConfig)
-#Determine from PKG
-if( PKG_CONFIG_FOUND AND NOT FFTW_ROOT )
-    pkg_check_modules( PKG_FFTW QUIET "fftw3" )
-endif()
-#Check whether to search static or dynamic libs
-set( CMAKE_FIND_LIBRARY_SUFFIXES_SAV ${CMAKE_FIND_LIBRARY_SUFFIXES} )
-if( ${FFTW_USE_STATIC_LIBS} )
-    set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX} )
-else()
-    set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_SHARED_LIBRARY_SUFFIX} )
-endif()
-if( FFTW_ROOT )
-    #find libs
-    find_library(
-        FFTW_LIB
-        NAMES "fftw3"
-        PATHS ${FFTW_ROOT}
-        PATH_SUFFIXES "lib" "lib64"
-        NO_DEFAULT_PATH
-        )
-    find_library(
-        FFTWF_LIB
-        NAMES "fftw3f"
-        PATHS ${FFTW_ROOT}
-        PATH_SUFFIXES "lib" "lib64"
-        NO_DEFAULT_PATH
-        )
-    find_library(
-        FFTWL_LIB
-        NAMES "fftw3l"
-        PATHS ${FFTW_ROOT}
-        PATH_SUFFIXES "lib" "lib64"
-        NO_DEFAULT_PATH
-        )
-    #find includes
-    find_path(
-        FFTW_INCLUDES
-        NAMES "fftw3.h"
-        PATHS ${FFTW_ROOT}
-        PATH_SUFFIXES "include"
-        NO_DEFAULT_PATH
-        )
-else()
-    find_library(
-        FFTW_LIB
-        NAMES "fftw3"
-        PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-        )
-    find_library(
-        FFTWF_LIB
-        NAMES "fftw3f"
-        PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-        )
-    find_library(
-        FFTWL_LIB
-        NAMES "fftw3l"
-        PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-        )
-    find_path(
-        FFTW_INCLUDES
-        NAMES "fftw3.h"
-        PATHS ${PKG_FFTW_INCLUDE_DIRS} ${INCLUDE_INSTALL_DIR}
-        )
-endif( FFTW_ROOT )
-set(FFTW_LIBRARIES ${FFTW_LIB} ${FFTWF_LIB})
-if(FFTWL_LIB)
-    set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWL_LIB})
-endif()
-set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES_SAV} )
+
+set(FFTW_INCLUDE_SEARCH_DIRS ${FFTW_ROOT}/include)
+set(FFTW_LIBRARY_SEARCH_DIRS ${FFTW_ROOT}/lib)
+
+set(FFTW_KNOWN_VERSIONS "3.3.5")
+
+set(FFTW_PATH_SUFFIXES)
+foreach(FFTW_VERSION ${FFTW_KNOWN_VERSIONS})
+  list(APPEND FFTW_PATH_SUFFIXES "fftw-${FFTW_VERSION}")
+endforeach()
+
+find_path(FFTW_INCLUDE_DIRS
+  NAMES         fftw3.h
+  PATHS         ${FFTW_INCLUDE_SEARCH_DIRS}
+  PATH_SUFFIXES ${FFTW_PATH_SUFFIXES}
+  NO_DEFAULT_PATH)
+
+find_library(FFTW_LIBRARY_DIRS
+  NAMES         fftw3
+  PATHS         ${FFTW_LIBRARY_SEARCH_DIRS}
+  PATH_SUFFIXES ${FFTW_PATH_SUFFIXES}
+  NO_DEFAULT_PATH)
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FFTW DEFAULT_MSG
-    FFTW_INCLUDES FFTW_LIBRARIES)
-mark_as_advanced(FFTW_INCLUDES FFTW_LIBRARIES FFTW_LIB FFTWF_LIB FFTWL_LIB)
+find_package_handle_standard_args(FFTW DEFAULT_MSG FFTW_INCLUDE_DIRS FFTW_LIBRARY_DIRS)
+
+mark_as_advanced(FFTW_INCLUDE_DIRS FFTW_LIRARY_DIRS)
+
+if(FFTW_FOUND)
+  add_library(FFTW::FFTW INTERFACE IMPORTED)
+  set_target_properties(FFTW::FFTW PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${FFTW_INCLUDE_DIRS})
+  set_target_properties(FFTW::FFTW PROPERTIES INTERFACE_LINK_LIBRARIES ${FFTW_LIBRARY_DIRS})
+
+endif()
