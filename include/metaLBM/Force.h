4170dd84cf4d772b4a40b26277ec9d3607ff21f7
tauzin <guillaumetauzin.ut@gmail.com>
2018-07-15 10:37:05 +0200

    [wip] NaN issue still present in energy removal

--- include/metaLBM/Force.h
+++ include/metaLBM/Force.h
@@ -495,6 +495,25 @@ namespace lbm {
 
             fourierForcePtr_iD[index][p::Re] = 0.0;
             fourierForcePtr_iD[index][p::Im] = 0.0;
+
+            if (iFP[L::dimD - 1] == 0) {
+              iFP_symmetric[d::X] = gSD::sLength()[d::X] - iFP[d::X];
+              if (iFP_symmetric[d::X] < lFD::length()[d::X]) {
+                iFP_symmetric[d::Y] = gSD::sLength()[d::Y] - iFP[d::Y];
+                iFP_symmetric[L::dimD - 1] = 0;
+
+                iK_symmetric[d::X] =
+                  iFP_symmetric[d::X] + offset[d::X] <= gSD::sLength()[d::X] / 2
+                  ? iFP_symmetric[d::X] + offset[d::X]
+                  : iFP_symmetric[d::X] + offset[d::X] - gSD::sLength()[d::X];
+                iK_symmetric[d::Y] = iFP_symmetric[d::Y] <= gSD::sLength()[d::Y] / 2
+                  ? iFP_symmetric[d::Y] : iFP_symmetric[d::Y] - gSD::sLength()[d::Y];
+
+                auto index_symmetric = lFD::getIndex(iFP_symmetric);
+                fourierForcePtr_iD[index_symmetric][p::Re] = 0.0;
+                fourierForcePtr_iD[index_symmetric][p::Im] = 0.0;
+              }
+            }
           }
 
           kNormSquared = iK.norm2();
