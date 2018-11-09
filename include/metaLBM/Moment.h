962a4e2a5e80f199a53ad89aa2fcd6fecbadd9b3
Guillaume Tauzin <guillaume.tauzin@gmail.com>
2018-11-09 09:06:58 +0100

    Corrected bug Regularization step

--- include/metaLBM/Moment.h
+++ include/metaLBM/Moment.h
@@ -47,6 +47,33 @@ class Moment {
   }
 
 
+
+  LBM_DEVICE LBM_HOST LBM_INLINE
+  static void calculateT2(const T* haloDistributionPreviousPtr, const T* haloDistributionNextPtr,
+                                   const Position& iP, T& T2) {
+    LBM_INSTRUMENT_OFF("Moment<T>::calculateT2", 5)
+
+    T non_equilibrium_0 = haloDistributionNextPtr[hSD::getIndex(iP, 0)];
+    T equilibrium_0 =
+      haloDistributionPreviousPtr[hSD::getIndex(iP - uiL::celerity()[0], 0)]
+      - haloDistributionNextPtr[hSD::getIndex(iP, 0)];
+
+    T2 = non_equilibrium_0 * non_equilibrium_0 / equilibrium_0;
+
+    #pragma unroll
+    for (auto iQ = 1; iQ < L::dimQ; ++iQ) {
+      T non_equilibrium_iQ = haloDistributionNextPtr[hSD::getIndex(iP, iQ)];
+      T equilibrium_iQ =
+        haloDistributionPreviousPtr[hSD::getIndex(iP - uiL::celerity()[iQ], iQ)]
+        - haloDistributionNextPtr[hSD::getIndex(iP, iQ)];
+
+      T2 += non_equilibrium_iQ * non_equilibrium_iQ / equilibrium_iQ;
+    }
+  }
+
+
+
+
   LBM_DEVICE LBM_HOST LBM_INLINE
   static void calculateObservables(const T* haloDistributionPreviousPtr, const T* haloDistributionNextPtr,
                                    const T density, const Position& iP, T& T2, T& T3, T& T4,
